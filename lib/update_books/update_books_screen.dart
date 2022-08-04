640fd8ccd678b0764cdd9c5c9d6dcf7bb67682a3
eunsang.kim <gomuzom@naver.com>
2022-08-05 03:18:09 +0900

    chore 수정취소버튼 추가

--- lib/update_books/update_books_screen.dart
+++ lib/update_books/update_books_screen.dart
@@ -73,28 +73,42 @@ class _UpdateBookScreenState extends State<UpdateBookScreen> {
                   hintText: widget.document['author'],
                 ),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  // 에러가 날 것 같은 코드
-                  viewModel
-                      .updateBook(
-                        document: widget.document,
-                        title: _titleTextController.text,
-                        author: _authorTextController.text,
-                        bytes: _bytes,
-                      )
-                      .then((_) => Navigator.pop(context))
-                      .catchError((e) {
-                    // 에러가 났을 때
-                    final snackBar = SnackBar(
-                      content: Text(e.toString()),
-                    );
-                    print(e);
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  });
-                },
-                child: const Text('도서 수정'),
-              )
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // 에러가 날 것 같은 코드
+                      viewModel
+                          .updateBook(
+                            document: widget.document,
+                            title: _titleTextController.text,
+                            author: _authorTextController.text,
+                            bytes: _bytes,
+                          )
+                          .then((_) => Navigator.pop(context))
+                          .catchError((e) {
+                        // 에러가 났을 때
+                        final snackBar = SnackBar(
+                          content: Text(e.toString()),
+                        );
+                        print(e);
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                      });
+                    },
+                    child: const Text('도서 수정'),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('수정 취소'),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
